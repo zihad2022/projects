48621fe5154820733f19b006aa740bfbc1f9aa3d
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-23 13:00:42 +0600

    change status field type to integer field

--- database/migrations/2024_09_23_060913_create_tasks_table.php
+++ database/migrations/2024_09_23_060913_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->date('deadline')->nullable();
-            $table->string('status')->default('pending');
+            $table->unsignedInteger('status');
             $table->timestamps();
             $table->softDeletes();
         });
