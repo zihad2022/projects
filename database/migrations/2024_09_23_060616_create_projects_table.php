5cefa02be185a5e6ea8d5c7f3232854822c7dbec
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-23 12:07:18 +0600

    create project model and migration file

---
+++ database/migrations/2024_09_23_060616_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('budget', 10, 2)->default(0);
+            $table->decimal('advanced_money', 10, 2)->default(0);
+            $table->date('deadline')->nullable();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
