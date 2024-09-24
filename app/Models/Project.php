40da45f55210e132de5b7833cf91ba55ede03847
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-24 16:05:13 +0600

    define relation with user

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
     use HasFactory,SoftDeletes;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
