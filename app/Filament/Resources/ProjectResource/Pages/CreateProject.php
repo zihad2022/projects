3249353643f537a3f112bc9edc3dc414b123c3dc
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-24 16:04:55 +0600

    add user id automatically

--- app/Filament/Resources/ProjectResource/Pages/CreateProject.php
+++ app/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -3,10 +3,17 @@
 namespace App\Filament\Resources\ProjectResource\Pages;
 
 use App\Filament\Resources\ProjectResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateProject extends CreateRecord
 {
     protected static string $resource = ProjectResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = Auth::user()->id;
+
+        return $data;
+    }
 }
