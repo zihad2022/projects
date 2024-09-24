4b159565d6356e731ed5b07104e6b348eedc0910
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-24 17:25:40 +0600

    define status in casts

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\ProjectStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +11,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory,SoftDeletes;
+
+    protected $casts = [
+        'status' => ProjectStatus::class,
+    ];
 
     public function user(): BelongsTo
     {
