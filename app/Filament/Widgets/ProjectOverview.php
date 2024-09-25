009fb5d3a1ff54036164ea55894b3fd17e106eb0
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-25 08:52:12 +0600

    display overview

---
+++ app/Filament/Widgets/ProjectOverview.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\ProjectStatus;
+use App\Models\Project;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Projects', Project::where('user_id', Auth::id())->count())->description('All projects'),
+            Stat::make('Pending Projects', Project::where('user_id', Auth::id())->where('status', ProjectStatus::Pending->value)->count())->description('All pnending projects'),
+            Stat::make('In Progress Projects', Project::where('user_id', Auth::id())->where('status', ProjectStatus::InProgress->value)->count())->description('All in progress projects'),
+            Stat::make('Completed Projects', Project::where('user_id', Auth::id())->where('status', ProjectStatus::Completed->value)->count())->description('All completed projects'),
+
+            Stat::make('Total Budget', Project::where('user_id', Auth::id())->sum('budget'))->description('All projects budget'),
+            Stat::make('Total Earned', Project::where('user_id', Auth::id())->sum('advanced_money'))->description('All projects earned money'),
+            Stat::make('Total Due', Project::where('user_id', Auth::id())->sum('due_money'))->description('All projects due money'),
+        ];
+    }
+}
