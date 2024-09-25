1bfeadae4651941099b5d940de54f22c0d977488
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-25 09:03:24 +0600

    change these navigation icon

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -23,7 +23,7 @@ class ProjectResource extends Resource
 {
     protected static ?string $model = Project::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/TaskResource.php
+++ app/Filament/Resources/TaskResource.php
@@ -23,7 +23,7 @@ class TaskResource extends Resource
 {
     protected static ?string $model = Task::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-check-circle';
 
     public static function form(Form $form): Form
     {
