223bc472aa5fe02808b30c3045af28353302687e
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-25 08:37:03 +0600

    redirect to index page

--- app/Filament/Resources/ProjectResource/Pages/CreateProject.php
+++ app/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -16,4 +16,9 @@ class CreateProject extends CreateRecord
 
         return $data;
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/ProjectResource/Pages/EditProject.php
+++ app/Filament/Resources/ProjectResource/Pages/EditProject.php
@@ -18,4 +18,9 @@ class EditProject extends EditRecord
             Actions\RestoreAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
