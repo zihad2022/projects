cfc73be428bdbb06463c3198614e45e75bce044b
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-25 10:22:21 +0600

    add tabs

--- app/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TaskResource\Pages;
 
+use App\Enums\TaskStatus;
 use App\Filament\Resources\TaskResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTasks extends ListRecords
 {
@@ -14,6 +17,31 @@ class ListTasks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+
+            'pending' => Tab::make('Pending')
+                ->icon('heroicon-o-clock')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TaskStatus::Pending->value);
+                }),
+
+            'in_progress' => Tab::make('In Progress')
+                ->icon('heroicon-o-play')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TaskStatus::InProgress->value);
+                }),
+
+            'completed' => Tab::make('Completed')
+                ->icon('heroicon-o-check-circle')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', TaskStatus::Completed->value);
+                }),
         ];
     }
 }
