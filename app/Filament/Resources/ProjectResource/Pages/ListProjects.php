c6f0c4fd1104492f9794f3e40a44dc66fa9877fb
wdevzihad <zihadulislamafnan@gmail.com>
2024-09-25 10:00:05 +0600

    add tabs

--- app/Filament/Resources/ProjectResource/Pages/ListProjects.php
+++ app/Filament/Resources/ProjectResource/Pages/ListProjects.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\ProjectResource\Pages;
 
+use App\Enums\ProjectStatus;
 use App\Filament\Resources\ProjectResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListProjects extends ListRecords
 {
@@ -14,6 +17,31 @@ class ListProjects extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+
+            'pending' => Tab::make('Pending')
+                ->icon('heroicon-o-clock')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', ProjectStatus::Pending->value);
+                }),
+
+            'in_progress' => Tab::make('In Progress')
+                ->icon('heroicon-o-play')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', ProjectStatus::InProgress->value);
+                }),
+
+            'completed' => Tab::make('Completed')
+                ->icon('heroicon-o-check-circle')
+                ->modifyQueryUsing(function (Builder $query) {
+                    return $query->where('status', ProjectStatus::Completed->value);
+                }),
         ];
     }
 }
